c579e2dfdfc375bec6481acad4449beff7b5ed2d
Monika Mona <mona.mt.89@gmail.com>
2022-08-16 23:06:23 +0200

    add test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PhotosComponent } from './photos/photos.component';
     PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
